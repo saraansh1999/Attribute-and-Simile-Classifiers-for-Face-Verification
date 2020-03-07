02434a7785fc20c1755f481f836b207a06717eef
saraansh <saraanshtandon1999@gmail.com>
2020-03-07 16:44:28 +0530

    comments added

--- low_level.py
+++ low_level.py
@@ -9,7 +9,11 @@ faceCascade = cv2.CascadeClassifier(cascPath)
 img = "./Train_Set/Ahmet_Demir_1.jpg"
 
 def expand_bb(x, y, w, h, img):
+	
+	#factor by which to extend in all corners
 	factor = 0.1
+
+
 	tlx, tly, brx, bry = x, y, x+w, y+h
 	if tlx - math.floor(factor*w) >= 0:
 		tlx -= math.floor(factor*w)
@@ -27,18 +31,24 @@ def expand_bb(x, y, w, h, img):
 		bry += math.floor(factor*h)
 	else:
 		bry = img.shape[1]
+	
 	return tlx, tly, brx-tlx, bry-tly
 
 def low_level(img):
+	#extracting faces
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	faces = faceCascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,flags=cv2.CASCADE_SCALE_IMAGE)
+
+	#finding the largest face
 	sz = faces[:, 2] * faces[:, 3]
 	face_ind = np.argmax(sz)
 	x, y, w, h = faces[face_ind, :]
+
+	#expanding the bounding box
 	x, y, w, h = expand_bb(x, y, w, h, img)
-	print(x, y, w ,h)
-	plt.imshow(img[x:x+w, y:y+h, :])
-	plt.show()
+	# print(x, y, w ,h)
+	# plt.imshow(img[x:x+w, y:y+h, :])
+	# plt.show()
 
 	return img[x:x+w, y:y+h, :]
 
